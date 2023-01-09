d07ac3f03c7531f2220d6f46081434c15fc02a91
ymizrahi <yitzhak.tech@gmail.com>
2023-01-10 00:34:00 +0200

    building the BlogList component

--- components/blog/BlogList.tsx
+++ components/blog/BlogList.tsx
@@ -1,6 +1,6 @@
 import { ArrowUpRightIcon } from '@heroicons/react/24/solid'
 import Image from 'next/image'
-import urlFor from '../lib/urlFor'
+import urlFor from '../../lib/urlFor'
 import ClientSideRoute from './ClientSideRoute'
 
 type Props = {
@@ -13,20 +13,23 @@ export default function BlogList({ posts }: Props) {
       <hr className=" border-[#F7AB0A] mb-10" />
       <div className="grid grid-cols-1 md:grid-cols-2 px-10 gap-10 gap-y-16 pb-24">
         {posts.map((post) => (
-          <ClientSideRoute route={`/post/${post.slug.current}`} key={post._id}>
+          <ClientSideRoute
+            route={`/post/${post?.slug?.current}`}
+            key={post?._id}
+          >
             <div className="flex flex-col group cursor-pointer">
               <div className="relative w-full h-80 drop-shadow-xl group-hover:scale-105 transition-transform duration-200 ease-out">
                 <Image
                   className="object-cover object-left lg:object-center"
-                  src={urlFor(post.mainImage).url()}
-                  alt={post.author.name}
+                  src={urlFor(post?.mainImage).url()}
+                  alt={post?.author?.name}
                   fill
                 />
                 <div className="absolute bottom-0 w-full bg-opacity-20 bg-black backdrop-blur-lg rounded drop-shadow-lg text-white p-5 flex justify-between">
                   <div>
-                    <p className="font-bold">{post.title}</p>
+                    <p className="font-bold">{post?.title}</p>
                     <p>
-                      {new Date(post._createdAt).toLocaleDateString('en-US', {
+                      {new Date(post?._createdAt).toLocaleDateString('en-US', {
                         day: 'numeric',
                         month: 'long',
                         year: 'numeric',
@@ -35,12 +38,12 @@ export default function BlogList({ posts }: Props) {
                   </div>
 
                   <div className="flex flex-col md:flex-row gap-y-2 md:gap-x-2 items-center">
-                    {post.categories.map((category) => (
+                    {post?.categories?.map((category) => (
                       <div
-                        key={category._id}
+                        key={category?._id}
                         className="bg-[#F7AB0A] text-center text-black px-3 py-1 rounded-full text-sm font-semibold"
                       >
-                        {category.title}
+                        {category?.title}
                       </div>
                     ))}
                   </div>
@@ -48,8 +51,10 @@ export default function BlogList({ posts }: Props) {
               </div>
 
               <div className="mt-5 flex-1">
-                <p className="underline text-lg font-bold">{post.title}</p>
-                <p className="line-clamp-2 text-gray-500">{post.description}</p>
+                <p className="underline text-lg font-bold">{post?.title}</p>
+                <p className="line-clamp-2 text-gray-500">
+                  {post?.description}
+                </p>
               </div>
 
               <p className="mt-5 font-bold flex items-center group-hover:underline">
