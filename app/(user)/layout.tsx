a925960b0b91e617d0887ee393f1d01414e59e2f
ymizrahi <yitzhak.tech@gmail.com>
2023-01-10 00:08:15 +0200

    implementing preview mode

---
+++ app/(user)/layout.tsx
@@ -0,0 +1,20 @@
+import '../../styles/globals.css'
+import Header from './../../components/blog/Header'
+import Banner from './../../components/blog/Banner'
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html>
+      <body>
+        <Header />
+        <Banner />
+
+        {children}
+      </body>
+    </html>
+  )
+}
--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,12 +1,35 @@
-import React from 'react'
+import { previewData } from 'next/headers'
+import { groq } from 'next-sanity'
+import { client } from './../../lib/sanity.client'
+import PreviewSuspense from '../../components/blog/PreviewSuspense'
+import PreviewBlogList from './../../components/blog/PreviewBlogList';
+import BlogList from './../../components/blog/BlogList';
 
-function HomePage() {
-  return (
-    <div>
-      <h1 className="text-4xl">Welcome to the Blog</h1>
-      <p>Some content</p>
-    </div>
-  )
+import { SpeakerWaveIcon } from '@heroicons/react/24/solid'
+const query = groq`*[_type=='post'] {
+  ...,
+  author->,
+  categories[]->
+} | order(_createdAt desc)
+`
+
+export default async function IndexPage() {
+  if (previewData()) {
+    return (
+      <PreviewSuspense
+        fallback={
+          <div role="status">
+            <p className="text-center text-lg animate-pulse text-[#F7AB0A]">
+              Loading Preview Data...
+            </p>
+          </div>
+        }
+      >
+        <PreviewBlogList query={query} />
+      </PreviewSuspense>
+    )
+  }
+
+  const posts = await client.fetch(query)
+  return <BlogList posts={posts} />
 }
-
-export default HomePage
