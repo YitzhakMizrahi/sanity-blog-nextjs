911be3ca0567c8249061413b7615791362dab97b
ymizrahi <yitzhak.tech@gmail.com>
2023-01-09 21:52:57 +0200

    setting sanity config

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,17 +1,18 @@
-import {defineConfig} from 'sanity'
-import {deskTool} from 'sanity/desk'
-import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemas'
+import { defineConfig } from 'sanity'
+import { deskTool } from 'sanity/desk'
+import { visionTool } from '@sanity/vision'
+import { schemaTypes } from './schemas'
+
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!
 
 export default defineConfig({
-  name: 'default',
-  title: 'nextjs13 sanity blog',
-
-  projectId: '7ts6xpra',
-  dataset: 'production',
-
+  basePath: '/studio',
+  name: 'YM_Content_Studio',
+  title: 'YM Content Studio',
+  projectId,
+  dataset,
   plugins: [deskTool(), visionTool()],
-
   schema: {
     types: schemaTypes,
   },
